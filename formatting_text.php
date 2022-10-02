6894faaaf62ddc0868c3af76e2814cfb34674afe
ihsantech07 <ihsantech07@gmail.com>
2022-10-02 18:29:23 +0700

    variable & tipe data , formatting text update

--- formatting_text.php
+++ formatting_text.php
@@ -1,5 +1,16 @@
 <?php
 
-$txt = "petanikode.com";
-printf("Aku belajar pemrograman PHP di %s<br>", $txt);
+$txt = "kampus";
+printf("Aku belajar pemrograman PHP di %s <br>", $txt);
+
+// %d untuk bilangan desimal (integer);
+// %f untuk pecahan (float);
+// %b untuk boolean.
+
+$harga = 100000;
+// jika kita cetak dengan echo:
+echo "Harga  barangnya adalah Rp $harga";
+
+// jika kita cetak dengan printf
+printf("Harga barangnya adalah Rp %.2f", $harga);
 
---
+++ variable_dan_tipedata.php
@@ -0,0 +1,30 @@
+
+<?php
+
+// tipe data char dan varchar (karakter/string)
+$jenis_kelamin = 'L';
+
+// tipe data string (teks)
+$nama_lengkap = "Ihsan Shalihin";
+
+// tipe data integer
+$umur = 20;
+
+// tipe data float
+$berat = 68.3;
+
+// tipe data float
+$tinggi = 172.2;
+
+// tipe data boolean
+$menikah = false;
+
+$tanggal_input = date('Y-m-d');
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "menikah: $menikah";
+echo "tanggal input : $tanggal_input";
